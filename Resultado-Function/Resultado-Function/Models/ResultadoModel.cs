926c1fc46091e19089081922f5ea3245d2b4614d
Tatiana <tatiana_florez@usmp.pe>
2023-05-07 18:04:27 -0500

    Resultados v1

---
+++ Resultado-Function/Resultado-Function/Models/ResultadoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resultado_Function.Models
+{
+    internal class ListResultadoModel
+    {
+        public int Id { get; set; }
+        public decimal? Velocidad { get; set; }
+        public decimal? Cadencia { get; set; }
+
+        public int IdUsuario { get; set; }
+
+
+    }
+}
