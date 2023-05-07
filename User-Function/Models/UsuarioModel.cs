e57c1db5bfe1dbd987b858e607523de5ef7d4bb3
Tatiana <tatiana_florez@usmp.pe>
2023-05-07 17:51:15 -0500

    Resultadosv1
    Resultados-Functionv1

---
+++ User-Function/Models/UsuarioModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Function.Models
+{
+    internal class UsuarioModel
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Correo { get; set; }
+
+        public string? Contraseña { get; set; }
+    }
+
+    public class ListUserModel
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Correo { get; set; }
+    }
+
+    public class CreateUserModel
+    {
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Correo { get; set; }
+
+        public string? Contraseña { get; set; }
+    }
+}
